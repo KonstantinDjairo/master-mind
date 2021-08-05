b6db9770984389624f3e4539f559144c424c1280
Mateus Rodrigues <mateus.rodrigues.sistema@gmail.com>
2021-08-05 16:02:09 -0300

    versão v1.0.0

--- apps/index/views.py
+++ apps/index/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
-
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 from ..bot.models import DoneList, Profile
 
 
 def index(request):
-    result = DoneList.objects.all()
-    
-    return render(request, "index/index.html", {"result": result})
+
+    object_list = Profile.objects.all()
+    paginator = Paginator(object_list, 1)
+    page = request.GET.get('page')
+    try:
+        profiles = paginator.page(page)
+    except PageNotAnInteger:
+
+        profiles = paginator.page(1)
+    except EmptyPage:
+
+        profiles = paginator.page(paginator.num_pages)
+    return render(request, "index/index.html", {'page': page, 'profiles': profiles})
 
 
-def player_detail(request, id_user):
-    player = get_object_or_404(Profile, id_user=id_user)
-    # terminar
+
